950979307ff801fdda5fff6fa95802a45047ef6a
Heike Hofmann <hofmann@iastate.edu>
2013-06-29 22:46:49 -0500

    data separated

---
+++ R/data.r
@@ -0,0 +1,18 @@
+#' Sequence data
+#' 
+#' @name sequences
+#' @title peptide sequence data
+#' @description available through biovis redesign contest 2013, see http://www.biovis.net/year/2013/info/redesign-contest
+#' published in Wong, B. Nat Methods 7, 889 (2011)
+#' @docType data
+#' @usage data(sequences)
+NULL
+
+#' alphabet polygon data is based on upper case letters from Helvetica.
+#' 
+#' @name alphabet
+#' @title alphabet polygon data
+#' @description polygons of all upper case letters 
+#' @docType data
+#' @usage data(alphabet)
+NULL
--- R/logos.r
+++ R/logos.r
@@ -48,21 +48,3 @@ calcInformation <- function(dframe, trt=NULL, pos, elems, k=4) {
   freqByPos
 }
 
-#' Sequence data
-#' 
-#' @name sequences
-#' @title peptide sequence data
-#' @description available through biovis redesign contest 2013, see http://www.biovis.net/year/2013/info/redesign-contest
-#' published in Wong, B. Nat Methods 7, 889 (2011)
-#' @docType data
-#' @usage data(sequences)
-NULL
-
-#' alphabet polygon data is based on upper case letters from Helvetica.
-#' 
-#' @name alphabet
-#' @title alphabet polygon data
-#' @description polygons of all upper case letters 
-#' @docType data
-#' @usage data(alphabet)
-NULL
