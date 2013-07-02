acbecb85390e2d73c52ac82ad49deda91d330c09
Heike Hofmann <hofmann@iastate.edu>
2013-07-01 22:22:43 -0500

    stat logo not found ...

--- R/logos.r
+++ R/logos.r
@@ -103,9 +103,10 @@ logo <- function(dm) {
 #' ggplot(dm3, aes(x=position, y=elinfo, group=element, label=element, fill=element)) + geom_logo() + scale_fill_manual(values=cols)
 #' dm4 <- calcInformation(dm2, pos="position", elems="element", trt="class", k=21)
 #' ggplot(dm4, aes(x=class, y=elinfo, group=element, label=element, fill=element), alpha=0.8) + geom_logo() + scale_fill_manual(values=scales::alpha(cols, 0.8)) + facet_wrap(~position, ncol=18)
+#' ggplot(dm4, aes(x=position, y=elinfo, group=element, label=element, fill=element), alpha=0.8) + geom_logo() + scale_fill_manual(values=scales::alpha(cols, 0.8)) + facet_wrap(~class, ncol=1) + theme_bw()
 #' }
 
-geom_logo <- function (mapping = NULL, data = NULL, stat = "logo", position = "identity", width = 0.9, alpha=0.9,
+geom_logo <- function (mapping = NULL, data = NULL, stat = "logo", position = "identity", width = 0.9, alpha=0.25,
                        ...) {
   GeomLogo$new(mapping = mapping, data = data, stat = stat, 
                position = position, width= width, ...)
@@ -115,34 +116,23 @@ GeomLogo <- proto(ggplot2:::Geom, {
   objname <- "logo"
   
   reparameterise <- function(., df, params) {
-    #     print("reparameterise")
-    #     browser()
-    #     
-    #     df$width <- df$width %||% 
-    #       params$width %||% (resolution(df$x, FALSE) * 0.9)
-    #     
-    #     # ymin, ymax, xmin, and xmax define the bounding rectangle for each group
-    #     ddply(df, .(group), transform,
-    #           ymin = min(y),
-    #           ymax = max(y),
-    #           xmin = x - width / 2,
-    #           xmax = x + width / 2)
     df
   }
   
   draw <- function(., data = data, scales, coordinates, ...) { 
 #    print("draw")
     
-    common <- unique(data.frame(
-      colour = "black", # data$colour, 
-      size = data$size, 
-      linetype = data$linetype,
-      fill = "white", #alpha(data$fill, data$alpha),  
-      stringsAsFactors = FALSE
-    ))
+#     common <- unique(data.frame(
+#       colour = data$colour, 
+#       size = data$size, 
+#       linetype = data$linetype,
+#       fill = "white", #alpha(data$fill, data$alpha),  
+#       stringsAsFactors = FALSE
+#     ))
+    data(alphabet)
     letter <- subset(alphabet, group %in% unique(data$label))
     if (nrow(letter) < 1) {
-      warning(paste("unrecognized letter in alphabet:",unique(data$label), collapse=","))
+      warning(paste("unrecognized letter in alphabet:", unique(data$label), collapse=","))
       letter <- alphabet[1,]
     }
     data$ROWID <- 1:nrow(data)
@@ -152,17 +142,21 @@ GeomLogo <- proto(ggplot2:::Geom, {
       letter$group <- interaction(x$ROWID, letter$group)
       letter
     })
-#   browser()
-#    row.names(common) <- NULL
-#    letterpoly <- data.frame(letterpoly, common)
-    letterpoly$fill <- alpha("black", 0.75)
-    
-    
+#  browser()
+
+    letterpoly$fill <- alpha("black", 0.7) # alpha(letterpoly$fill, letterpoly$alpha) #"white" 
+    letterpoly$colour <- NA #"white"
+    letterpoly$size <- 0.5    
+# for drawing paths
+    letterOutline <- letterpoly
+    letterOutline$colour <- alpha("black", letterOutline$alpha)
+    letterOutline$group <- paste(letterOutline$group, letterOutline$pathGroup, sep=".")
     
     ggname(.$my_name(), 
            gTree(children=gList(
              GeomRect$draw(data, scales, coordinates, ...),
-             GeomPolygon$draw(letterpoly, scales, coordinates, ...)
+             GeomPolygon$draw(letterpoly, scales, coordinates, ...) #,
+       #      GeomPath$draw(letterOutline, scales, coordinates, ...)
            ))
     )    
   }
@@ -185,47 +179,8 @@ GeomLogo <- proto(ggplot2:::Geom, {
   
 })
 
-
-
 #' calculation of all pieces necessary to plot a logo sequence plot
 #' 
-#' @param df dataframe
-#' @examples
-#' dmlabel <- stat_logo(dm3)
-#' dmlabel$x <- as.numeric(dmlabel$position)-0.4
-#' dmlabel$xmax <- dmlabel$x + 0.8
-#' 
-#' ## very long example - should be in the code not an example
-#' cols <- biovizBase::getBioColor(type="AA_ALPHABET")
-#' #' 
-#' #' dm4 <- calcInformation(dm2, pos="position", trt=NULL, elems="element", k=21)
-#' dm4$class <- 1
-#' dmlabel <- stat_logo(dm4)
-#' dmlabel$x <- as.numeric(dmlabel$position)-0.4
-#' dmlabel$xmax <- dmlabel$x+0.8
-#' base <- ggplot(aes(x, y), data=dmlabel) +
-#'   geom_rect(aes(xmin=x, xmax=xmax, ymin=y, ymax=ymax, colour=element, fill=element)) + 
-#'   scale_fill_manual(values=cols) + 
-#'   scale_colour_manual(values=cols) + 
-#'   theme(legend.position="bottom") + 
-#'   geom_hline(yintercept=0, colour="grey20") + 
-#'   geom_hline(yintercept=-log(1/21, base=2), colour="grey20") +
-#'   scale_x_continuous("position", breaks=1:39, labels=1:39)
-#' ylab("bits")
-#' 
-#' # use Biovisbase for colors
-#' data(alphabet)
-#' dmletter <- merge(subset(dmlabel, elinfo > 0.25), alphabet, by.x="element", by.y="group")
-#' 
-#' base + geom_polygon(aes(x=x.x+x.y-0.1, y=y.x+elinfo*y.y, group=interaction(position, element), order=order), 
-#'                     alpha=0.9, fill="black", data=dmletter, 
-#'                     guides="none") + 
-#'   scale_shape_identity() + 
-#'   scale_size(range=6*c(0.5, max(dmlabel$freq)), guide="none")
-#' combination of boxplot and 1d kernel density estimate along y axis, for logo plot.
-#'
-
-#' @section Aesthetics: 
 #'
 #' @param scale if "area" (default), all vases have the same area (before trimming
 #'   the tails). If "count", areas are scaled proportionally to the number of
@@ -248,11 +203,10 @@ GeomLogo <- proto(ggplot2:::Geom, {
 #' @examples
 #' # See geom_logo for examples
 #' # Generate data
-#' data(peptide)
+#' data(sequences)
 #' dm2 <- splitSequence(sequences, "peptide")
 #' dm3 <- calcInformation(dm2, pos="position", elems="element", k=21)
 #' ggplot(dm3, aes(x=position, y=elinfo, group=interaction(position, element))) + geom_logo()
-
 stat_logo <- function (mapping = NULL, data = NULL, geom = "logo", position = "identity",
                        width = 0.9, drop="FALSE", scale = "area", na.rm = FALSE, ...) {
   StatLogo$new(mapping = mapping, data = data, geom = geom, position = position,
